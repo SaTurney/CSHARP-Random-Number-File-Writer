f9043d8feba570203416a8de08bdcff337ed66ad
Sabrina Turney <turney.sabrina@gmail.com>
2020-01-03 20:24:39 -0500

    Add files via upload

---
+++ Random Number File Writer/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Random_Number_File_Writer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.instructionLabel = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.saveToFile = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // instructionLabel
+            // 
+            this.instructionLabel.AutoSize = true;
+            this.instructionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.instructionLabel.Location = new System.Drawing.Point(16, 28);
+            this.instructionLabel.Name = "instructionLabel";
+            this.instructionLabel.Size = new System.Drawing.Size(377, 60);
+            this.instructionLabel.TabIndex = 0;
+            this.instructionLabel.Text = "Please enter a number 1 and 100 \r\nand you will get a saved file of that number \r\n" +
+    "comprised of random numbers!";
+            this.instructionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(275, 260);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(156, 72);
+            this.exitButton.TabIndex = 1;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // saveToFile
+            // 
+            this.saveToFile.Location = new System.Drawing.Point(46, 260);
+            this.saveToFile.Name = "saveToFile";
+            this.saveToFile.Size = new System.Drawing.Size(156, 72);
+            this.saveToFile.TabIndex = 2;
+            this.saveToFile.Text = "Save To File";
+            this.saveToFile.UseVisualStyleBackColor = true;
+            this.saveToFile.Click += new System.EventHandler(this.SaveToFile_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.groupBox1.Controls.Add(this.inputTextBox);
+            this.groupBox1.Controls.Add(this.instructionLabel);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(477, 212);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(115, 118);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(183, 22);
+            this.inputTextBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 378);
+            this.Controls.Add(this.saveToFile);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Random Number File Writer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label instructionLabel;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button saveToFile;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Random Number File Writer/Form1.cs
@@ -0,0 +1,76 @@
+﻿//Sabrina Turney
+//C# I
+//Assignment Chapter 5.13 - Rnadom Number File Writer
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//Including System to use Save option and Write options
+using System.IO;
+
+namespace Random_Number_File_Writer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SaveToFile_Click(object sender, EventArgs e)
+        {
+            try //starting off with a try block for writing and saving full input
+            {
+                //Using Streamwriter to get an easier variable to use first off!
+                StreamWriter savedFile;
+                int amt = int.Parse(inputTextBox.Text.ToString());
+                //taking the string entered by the user, using parsing to change it to a usable calculaiton
+                //this allows us to iterate.
+                int randNum = 0;
+                int iteration = 1;
+
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    savedFile = File.CreateText(saveFileDialog1.FileName);
+                    Random rand = new Random();
+
+                    while (iteration <= amt) 
+                        //This while block does the work, iterating through each number entered by the user
+                        //Then writing a random number to the numbers the user gave into a new file.
+                    {
+                        randNum = rand.Next(100) + 1;
+                        savedFile.WriteLine(randNum);
+                        iteration++;
+                    }
+                    //IMPORTANT: Close a file once you're done with it. 
+                    savedFile.Close();
+                }//catches if the file does not save correctly to the savedialogbox in form designer.
+                else
+                {
+                    MessageBox.Show("Error: Your file could not save.");
+                }
+            }//actual catch block to catch anything except the requested integers:
+            catch
+            {
+                MessageBox.Show("Please enter a whole integer between 1 and 100.");
+            }
+        }
+        //Closing the program, my personal favorite part. Hard to mess this event up.
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
